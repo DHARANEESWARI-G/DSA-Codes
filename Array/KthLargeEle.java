a5590b0228bffb9ed577956e3ef99ba6e054e0ec
DHARANEESWARI-G <dhar22027.cs@rmkec.ac.in>
2024-01-12 22:01:25 +0530

    new problem code

---
+++ Array/KthLargeEle.java
@@ -0,0 +1,24 @@
+//DSA java program given an integer k and an array of size n consisting of unique integers.
+// Find the kth largest element in this array.
+
+import java.util.*;
+
+public class KthLargeEle {
+
+    public static int KthLargestElement(int arr[], int n, int k) {
+
+        // sorting the array
+        Arrays.sort(arr);
+
+        // return kth largest element
+        return arr[n - k];
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 2, 1, 4, 6, 3, 9, 7 };
+        int n = arr.length;
+        int k = 2;
+        int x = KthLargestElement(arr, n, k);
+        System.out.print("Kth largest element is " + x);
+    }
+}
