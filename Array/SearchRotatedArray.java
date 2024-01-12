19363cbadc7372fd92e1c6702199753dad7dccf5
DHARANEESWARI-G <dhar22027.cs@rmkec.ac.in>
2024-01-12 14:08:47 +0530

    array input changed

--- Array/SearchRotatedArray.java
+++ Array/SearchRotatedArray.java
@@ -17,18 +17,7 @@ public class SearchRotatedArray {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		// user input to get the size of the array
-		System.out.print("Enter the size of the array:" + " ");
-		int size = sc.nextInt();
-		int[] arr = new int[size];
-
-		// input of the array elements separated by space
-		System.out.print("Enter the elements of the array:" + " ");
-		for (int i = 0; i < size; i++) {
-			arr[i] = sc.nextInt();
-		}
-
-		// key to be searched
+		int[] arr = { 5, 6, 7, 8, 1, 2, 3, 4 };
 		System.out.print("Enter the key to be found:" + " ");
 		int key = sc.nextInt();
 
